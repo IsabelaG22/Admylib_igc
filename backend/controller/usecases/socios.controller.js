79e9008d83a1c78381f7f2d166f6e9f75e8be1e5
Isabela Grisales castrillón <igrisales01@soy.sena.edu.co>
2023-09-17 11:20:53 -0500

    se creo CRUD de socios

--- backend/controller/usecases/socios.controller.js
+++ backend/controller/usecases/socios.controller.js
@@ -10,10 +10,34 @@ const modelSocio = require('../../models/socios.model');
 
 // };
 
-exports.guardar = async (req, res)=>{
+exports.guardar = async (req, res) => {
   const datos = req.body;
-  const nuevoSocios = await dataSocio.guardar(datos);
-  console.log(nuevoSocios);
+  const buscarExistencia = await dataSocio.buscar({cedula: req.body.cedula});
+  if (buscarExistencia == req.body.cedula) {
+    res.status(500).json({socio: 'usuario ya existe '});
+  } else {
+    const nuevoSocios = await dataSocio.guardar(datos);
+    res.status(200).json({socio: 'socio guardado con exito' + nuevoSocios});
+  }
+};
+
+exports.buscar = async (req, res) => {
+  const nombre = {nombre: req.body.nombre};
+  const busquedaSocio = await dataSocio.buscar(nombre);
+  res.status(200).json({socio: busquedaSocio});
+};
+
+exports.eliminar = async (req, res) => {
+  const id = req.params.id;
+  const eliminarSocio = await dataSocio.eliminar(id);
+  res.status(200).json({socio: 'se elimino con exito ' + eliminarSocio.nombre});
+};
+
+exports.actualizar = async (req, res) => {
+  const id = {_id: req.params.id};
+  const actualizar = req.body;
+  const actualizarSocio = await dataSocio.actualizar(id, actualizar);
+  res.status(200).json({socio: 'se actualizo con exito'});
 };
 
 exports.index = (req, res) => {
--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 router.get('/index', funcionesSocio.index);
 router.post('/agregarSocio', funcionesSocio.guardar);
+router.get('/buscarSocio', funcionesSocio.buscar);
+router.get('/eliminarSocio/:id', funcionesSocio.eliminar);
+router.post('/actualizarSocio/:id', funcionesSocio.actualizar);
+
 
 module.exports= router;
